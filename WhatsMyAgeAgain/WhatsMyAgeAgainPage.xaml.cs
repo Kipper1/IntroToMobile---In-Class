8ad8ebe8cbd6976c45f34bf92c8b417171044f14
Kipper1 <caseykelpinski@gmail.com>
2018-09-19 18:56:52 -0500

    Initial Commit class example

---
+++ WhatsMyAgeAgain/WhatsMyAgeAgainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using System;
+
+
+namespace WhatsMyAgeAgain
+{
+    public partial class WhatsMyAgeAgainPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var month = int.Parse(MonthEntry.Text);
+            var day = int.Parse(DayEntry.Text);
+            var year = int.Parse(YearEntry.Text);
+
+            var birthDate = new DateTime(year, month, day);
+            var age = DateTime.Now.Date.Subtract(birthDate);
+
+            var yearsAlive = age.Days / 365;
+
+            OutputLabel.Text = yearsAlive.ToString();
+            
+        }
+
+        public WhatsMyAgeAgainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
